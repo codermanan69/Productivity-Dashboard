8c42fb3727fdc27d148643cb6864003dc8e64ca6
codermanan69 <manansahni295@gmail.com>
2026-02-19 02:24:57 +0530

    com

--- script.js
+++ script.js
@@ -1,155 +1,144 @@
-function openfeatures() {
+
+function openFeatures() {
     var allElems = document.querySelectorAll('.elem')
     var fullElemPage = document.querySelectorAll('.fullElem')
-    var fullElemPageBackbtn = document.querySelectorAll('.fullElem .back')
+    var fullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
+
     allElems.forEach(function (elem) {
         elem.addEventListener('click', function () {
             fullElemPage[elem.id].style.display = 'block'
-            localStorage.setItem('currentOpenPage', elem.id)
         })
     })
-    fullElemPageBackbtn.forEach(function (back) {
+
+    fullElemPageBackBtn.forEach(function (back) {
         back.addEventListener('click', function () {
             fullElemPage[back.id].style.display = 'none'
-            localStorage.removeItem('currentOpenPage')
         })
-
     })
-    
-    // Restore the page on reload
-    var savedPage = localStorage.getItem('currentOpenPage')
-    if (savedPage !== null) {
-        fullElemPage[savedPage].style.display = 'block'
-    }
 }
-openfeatures()
 
-function todolist() {
-    let form = document.querySelector('.addtask form')
-    let taskInput = document.querySelector('.addtask #task-input')
-    let taskDetailsInput = document.querySelector('.addtask textarea')
-    let taskcheckbox = document.querySelector('.addtask form input[type="checkbox"]')
+openFeatures()
+
+
+function todoList() {
 
     var currentTask = []
 
     if (localStorage.getItem('currentTask')) {
         currentTask = JSON.parse(localStorage.getItem('currentTask'))
     } else {
-        console.log('Task list is empty');
-
+        console.log('Task list is Empty');
     }
+
 
 
     function renderTask() {
 
-        let alltask = document.querySelector('.alltask');
+        var allTask = document.querySelector('.allTask')
 
-        let sum = ''
+        var sum = ''
 
-        currentTask.forEach(function (elem,idx) {
-            sum = sum + ` <div class="task">
-                        <h5>${elem.task} <span class= ${elem.imp}>imp </span> </h5>
-                        <button id= ${idx}>Mark as Completed </button>
-                        
-                    </div>`
-
+        currentTask.forEach(function (elem, idx) {
+            sum = sum + `<div class="task">
+        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+        <button id=${idx}>Mark as Completed</button>
+        </div>`
         })
 
-        alltask.innerHTML = sum
+        allTask.innerHTML = sum
+
         localStorage.setItem('currentTask', JSON.stringify(currentTask))
 
-       document.querySelectorAll('.task button').forEach(function (btn) {
+        document.querySelectorAll('.task button').forEach(function (btn) {
             btn.addEventListener('click', function () {
                 currentTask.splice(btn.id, 1)
-
                 renderTask()
-              
-
             })
         })
-
     }
-
     renderTask()
 
-
+    let form = document.querySelector('.addTask form')
+    let taskInput = document.querySelector('.addTask form #task-input')
+    let taskDetailsInput = document.querySelector('.addTask form textarea')
+    let taskCheckbox = document.querySelector('.addTask form #check')
 
     form.addEventListener('submit', function (e) {
         e.preventDefault()
-
-        //  console.log(taskInput.value);
-        //  console.log(taskDetailsInput.value);
-        //  console.log(taskcheckbox.checked);
-
-
-        currentTask.push({ task: taskInput.value, details: taskDetailsInput.value, imp: taskcheckbox.checked })
-
+        currentTask.push(
+            {
+                task: taskInput.value,
+                details: taskDetailsInput.value,
+                imp: taskCheckbox.checked
+            }
+        )
         renderTask()
 
-        taskcheckbox.checked = false 
+        taskCheckbox.checked = false
         taskInput.value = ''
         taskDetailsInput.value = ''
-
-
-
     })
+
 
 
 }
 
-// todolist()
+todoList()
+
 
 function dailyPlanner() {
     var dayPlanner = document.querySelector('.day-planner')
 
-var dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
+    var dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
 
-var hours = Array.from({length:18},(elem,idx)=>`${6+idx}:00 - ${7+idx}:00`)
-
-
-
-var wholedaysome = ''
-hours.forEach(function(elem,idx){
-
-     var savedData = dayPlanData[idx] || ''
-   wholedaysome = wholedaysome + `<div class="day-planner-time">
-            <p>${elem}</p>
-            <input id = ${idx} type="text" placeholder="..." value=${savedData}>
-           </div>`
-})
-
-dayPlanner.innerHTML = wholedaysome
+    var hours = Array.from({ length: 18 }, (_, idx) => `${6 + idx}:00 - ${7 + idx}:00`)
 
 
-var dayPlannerInput = document.querySelectorAll('.day-planner input')
+    var wholeDaySum = ''
+    hours.forEach(function (elem, idx) {
 
-dayPlannerInput.forEach(function(elem){
-    elem.addEventListener('input', function() {
-       dayPlanData[elem.id] = elem.value
-        
-       localStorage.setItem('dayPlanData', JSON.stringify(
-        dayPlanData
-       ))
-       
+        var savedData = dayPlanData[idx] || ''
+
+        wholeDaySum = wholeDaySum + `<div class="day-planner-time">
+    <p>${elem}</p>
+    <input id=${idx} type="text" placeholder="..." value=${savedData}>
+</div>`
     })
-})
 
+    dayPlanner.innerHTML = wholeDaySum
+
+
+    var dayPlannerInput = document.querySelectorAll('.day-planner input')
+
+    dayPlannerInput.forEach(function (elem) {
+        elem.addEventListener('input', function () {
+            console.log('hello');
+            dayPlanData[elem.id] = elem.value
+
+            localStorage.setItem('dayPlanData', JSON.stringify(dayPlanData))
+        })
+    })
 }
 
-dailyPlanner() 
+dailyPlanner()
 
-async function fetchQuote(){
 
-let response = await fetch('https://zenquotes.io/api/random')
-.then(res => res.json())
-  .then(data => {
-    console.log(data[0].q, data[0].a);
-  });
+function motivationalQuote() {
+    var motivationQuoteContent = document.querySelector('.motivation-2 h1')
+    var motivationAuthor = document.querySelector('.motivation-3 h2')
 
-console.log(response)
+    async function fetchQuote() {
+        let response = await fetch('https://api.quotable.io/random')
+        let data = await response.json()
+
+        motivationQuoteContent.innerHTML = data.content
+        motivationAuthor.innerHTML = data.author
+    }
+
+    fetchQuote()
 }
 
-fetchQuote()
+motivationalQuote()
 
 
 function pomodoroTimer() {
@@ -228,12 +217,107 @@ function pomodoroTimer() {
 }
 
 pomodoroTimer()
-window.addEventListener("load", () => {
-  // saare full pages hide
-  document.querySelectorAll(".fullElem").forEach(elem => {
-    elem.style.display = "none";
-  });
 
-  // home cards show
-  document.querySelector(".allElems").style.display = "flex";
-});
+
+
+function weatherFunctionality() {
+
+
+    var apiKey = null
+    var city = 'Gwalior'
+
+
+
+    var header1Time = document.querySelector('.header1 h1')
+    var header1Date = document.querySelector('.header1 h2')
+    var header2Temp = document.querySelector('.header2 h2')
+    var header2Condition = document.querySelector('.header2 h4')
+    var precipitation = document.querySelector('.header2 .precipitation')
+    var humidity = document.querySelector('.header2 .humidity')
+    var wind = document.querySelector('.header2 .wind')
+
+    var data = null
+
+    async function weatherAPICall() {
+        var response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+        data = await response.json()
+
+        header2Temp.innerHTML = `${data.current.temp_c}°C`
+        header2Condition.innerHTML = `${data.current.condition.text}`
+        wind.innerHTML = `Wind: ${data.current.wind_kph} km/h`
+        humidity.innerHTML = `Humidity: ${data.current.humidity}%`
+        precipitation.innerHTML = `Heat Index : ${data.current.heatindex_c}%`
+    }
+
+    weatherAPICall()
+
+
+    function timeDate() {
+        const totalDaysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+        const monthNames = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        var date = new Date()
+        var dayOfWeek = totalDaysOfWeek[date.getDay()]
+        var hours = date.getHours()
+        var minutes = date.getMinutes()
+        var seconds = date.getSeconds()
+        var tarik = date.getDate()
+        var month = monthNames[date.getMonth()]
+        var year = date.getFullYear()
+
+        header1Date.innerHTML = `${tarik} ${month}, ${year}`
+
+        if (hours > 12) {
+            header1Time.innerHTML = `${dayOfWeek}, ${String(hours - 12).padStart('2', '0')}:${String(minutes).padStart('2', '0')}:${String(seconds).padStart('2', '0')} PM`
+
+        } else {
+            header1Time.innerHTML = `${dayOfWeek}, ${String(hours).padStart('2', '0')}:${String(minutes).padStart('2', '0')}:${String(seconds).padStart('2', '0')} AM`
+        }
+    }
+
+    setInterval(() => {
+        timeDate()
+    }, 1000);
+
+}
+
+weatherFunctionality()
+
+
+function changeTheme() {
+
+    var theme = document.querySelector('.theme')
+    var rootElement = document.documentElement
+
+    var flag = 0
+    theme.addEventListener('click', function () {
+
+        if (flag == 0) {
+            rootElement.style.setProperty('--pri', '#F8F4E1')
+            rootElement.style.setProperty('--sec', '#222831')
+            rootElement.style.setProperty('--tri1', '#948979')
+            rootElement.style.setProperty('--tri2', '#393E46')
+            flag = 1
+        } else if (flag == 1) {
+            rootElement.style.setProperty('--pri', '#F1EFEC')
+            rootElement.style.setProperty('--sec', '#030303')
+            rootElement.style.setProperty('--tri1', '#D4C9BE')
+            rootElement.style.setProperty('--tri2', '#123458')
+            flag = 2
+        } else if (flag == 2) {
+            rootElement.style.setProperty('--pri', '#F8F4E1')
+            rootElement.style.setProperty('--sec', '#381c0a')
+            rootElement.style.setProperty('--tri1', '#FEBA17')
+            rootElement.style.setProperty('--tri2', '#74512D')
+            flag = 0
+        }
+
+    })
+
+
+}
+
+changeTheme()
